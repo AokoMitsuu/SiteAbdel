d36ad4fe86f46495475feb55043f4b82a9a5801d
esskaaa <nugierbatiste1@gmail.com>
2022-12-01 23:22:26 +0100

    Creation site

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+}>()
+</script>
+
+<template>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <img class="logo  " src="../assets/vin.png" alt="">
+      </div>
+      <div class="col-sm">
+          <button type="button" class="btn btn-transparent">Accueil</button>
+          <button type="button" class="btn btn-transparent">Catalogue</button>
+          <button type="button" class="btn btn-transparent">A propos</button>
+          <button type="button" class="btn btn-transparent">Où nous trouver</button>    
+      </div>
+      <div class="col-sm header-left">
+        <button  type="button" class="btn btn-transparent">Acheter</button>
+        <img class="logoBuy" src="../assets/Buy-Icon.png" alt="">
+      </div>
+    </div>
+  </div>
+
+
+
+
+</template>
+
+<style scoped>
+.logo{
+  height: 70px;
+}
+
+.logoBuy{
+  height: 35px;
+}
+
+
+
+</style>
